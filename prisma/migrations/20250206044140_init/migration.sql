3cb5caa078b573bf33abccdfd851396c6a19f66f
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-06 12:43:23 +0800

    test

---
+++ prisma/migrations/20250206044140_init/migration.sql
@@ -0,0 +1,69 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "donation" (
+    "id" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "specialMessage" TEXT NOT NULL,
+    "socialURLOrBuyMeACoffee" TEXT NOT NULL,
+    "donorId" INTEGER NOT NULL,
+    "recipentId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "donation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profile" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "avatarImage" TEXT NOT NULL,
+    "socialMediaURL" TEXT NOT NULL,
+    "backgroundImage" TEXT NOT NULL,
+    "successMessage" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bankCard" (
+    "id" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "cardNumber" TEXT NOT NULL,
+    "expiryDate" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "bankCard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "donation_userId_key" ON "donation"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profile_userId_key" ON "profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bankCard_userId_key" ON "bankCard"("userId");
+
+-- AddForeignKey
+ALTER TABLE "donation" ADD CONSTRAINT "donation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bankCard" ADD CONSTRAINT "bankCard_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
