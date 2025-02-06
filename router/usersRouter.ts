f743911a3210fea9638f02bdd37f3ccfb220393d
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-06 21:36:42 +0800

    signin signup response

--- router/usersRouter.ts
+++ router/usersRouter.ts
@@ -13,25 +13,59 @@ usersRouter.post("/addnew", async (req: Request, res: Response) => {
       },
     });
     if (existingUser) {
-      res.json({ message: "user already exist" });
+      res.json({ message: "user already exist", id: existingUser.id });
       return;
     }
     const rounds = 10;
     const encryptedPass = await bcrypt.hash(password, rounds);
-    await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: { email, password: encryptedPass, username },
     });
-    res.json({ message: "success" });
+    res.json({ message: "success", id: newUser.id });
   } catch (e) {
     console.error(e, "aldaa");
   }
 });
 // login
-usersRouter.get("/", async (req: Request, res: Response) => {
-  // const { email, password, username } = req.body;
+usersRouter.post("/auth/sign-in", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
   try {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (existingUser && password) {
+      const pass = await bcrypt.compare(password, existingUser.password);
+      if (pass) {
+        res.json({ message: "password matches", id: existingUser.id });
+      } else {
+        res.json({ message: "password didn't match" });
+      }
+    } else {
+      res.json({ message: "not registered" });
+    }
+  } catch (e) {
+    console.error(e, "aldaa");
+  }
+});
+usersRouter.post("/auth/:username", async (req: Request, res: Response) => {
+  const { username } = req.params;
+  if (username.length < 6) {
+    res.json({ message: "username is too short", no: "no" });
+    return;
+  }
+  try {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
+    if (existingUser) {
+      res.json({ message: "username is taken", no: "no" });
+    } else {
+      res.json({ message: "username is available", yes: "yes" });
+    }
   } catch (e) {
     console.error(e, "aldaa");
   }
