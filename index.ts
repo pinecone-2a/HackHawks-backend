31b97c6153cea513cbe2e190b61d865ce7e23ea1
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-06 16:30:03 +0800

    signin signup

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { bankCardRouter } from "./router/bankCardRouter";
 import { usersRouter } from "./router/usersRouter";
 import { configDotenv } from "dotenv";
 
 const cors = require("cors");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 configDotenv();
+
 app.use("/users", usersRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+
 app.use("/bankCards", bankCardRouter);
 const port = 4000;
 app.listen(port, () => {
--- router/usersRouter.ts
+++ router/usersRouter.ts
@@ -6,10 +6,28 @@ export const usersRouter = Router();
 usersRouter.post("/addnew", async (req: Request, res: Response) => {
   const { email, password, username } = req.body;
   try {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      res.json({ message: "user already exist" });
+      return;
+    }
     const newUser = await prisma.user.create({
       data: { email, password, username },
     });
-    res.json(newUser);
+    res.json({ message: "success" });
+  } catch (e) {
+    console.error(e, "aldaa");
+  }
+});
+usersRouter.get("/", async (req: Request, res: Response) => {
+  // const { email, password, username } = req.body;
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
   } catch (e) {
     console.error(e, "aldaa");
   }
