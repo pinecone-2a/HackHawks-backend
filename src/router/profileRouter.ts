9d4633290f6ef4e699838a19302806f11c2d22b5
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-07 23:10:24 +0800

    a

--- src/router/profileRouter.ts
+++ src/router/profileRouter.ts
@@ -6,11 +6,30 @@ export const profileRouter = Router();
 
 profileRouter.post("/", async (req: Request, res: Response) => {
   const body = req.body;
+  console.log(body);
   try {
     const newProfile = await prisma.profile.create({
       data: body,
     });
     res.json({ message: "success" });
+  } catch (e) {
+    console.error(e, "aldaa 1");
+  }
+});
+profileRouter.get("/", async (req: Request, res: Response) => {
+  // const { userId } = req.params;
+  try {
+    const profile = await prisma.profile.findMany({
+      include: {
+        user: true,
+      },
+    });
+    if (profile) {
+      res.json({ userExists: true, profile });
+      return;
+    } else {
+      res.json({ userExists: false });
+    }
   } catch (e) {
     console.error(e, "aldaa 1");
   }
