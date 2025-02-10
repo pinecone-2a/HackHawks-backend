7593949e893146d8214d93651357ecceda2b5316
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-02-10 20:43:55 +0800

    wokring on dashboard total2

--- src/controller/donation/TOTAL-donation.ts
+++ src/controller/donation/TOTAL-donation.ts
@@ -1,21 +1,21 @@
 export const totalDonation = async (req: Request, res: Response) => {
-  //   const { id } = req.params;
-  //   const amount = req.query.amount;
-  //   const days = req.query.days;
-  //   const lte = new Date();
-  //   const gte = new Date(lte);
-  //   const todayDay = lte.getMonth();
-  //   gte.setDate(todayDay - Number(days));
-  //   console.log(gte);
-  //   try {
-  //     const donation = await prisma.donation.findMany({
-  //       where: {
-  //         AND: [{ recipentId: id }, amount ? { amount: Number(amount) } : {}],
-  //         createdAt: { gte, lte },
-  //       },
-  //     });
-  //     res.json(donation);
-  //   } catch (e) {
-  //     console.error(e, "received donation error");
-  //   }
+  // const { id } = req.params;
+  // const amount = req.query.amount;
+  // const days = req.query.days;
+  // const lte = new Date();
+  // const gte = new Date(lte);
+  // const todayDay = lte.getMonth();
+  // gte.setDate(todayDay - Number(days));
+  // console.log(gte);
+  // try {
+  //   const donation = await prisma.donation.findMany({
+  //     where: {
+  //       AND: [{ recipentId: id }, amount ? { amount: Number(amount) } : {}],
+  //       createdAt: { gte, lte },
+  //     },
+  //   });
+  //   res.json(donation);
+  // } catch (e) {
+  //   console.error(e, "received donation error");
+  // }
 };
