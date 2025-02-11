deb263c760123cd994f164465916db827ab6edf8
24LP5392 24LP5392 <24LP5392@G304-CC0117.local>
2025-02-11 18:00:25 +0800

    viewpage-backend

--- src/router/usersRouter.ts
+++ src/router/usersRouter.ts
@@ -1,10 +1,10 @@
 import { Request, Response, Router } from "express";
 import { loginUser } from "../controller/user/CURRENT-profile";
 import { createUser } from "../controller/user/CREATE-account";
-import { checkUsername } from "../controller/user/check-username";
 import { prisma } from "..";
 import bcrypt from "bcrypt";
 import { generateToken, verifyToken } from "../controller/authorization/verify";
+import { checkUsername } from "../controller/user/CHECK-username";
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
@@ -15,6 +15,39 @@ usersRouter.post("/addnew", createUser);
 usersRouter.post("/auth/sign-in", loginUser);
 
 usersRouter.post("/auth/:username", checkUsername);
+
+usersRouter.get("/auth/:userId", async(req:Request, res: Response)=>{
+    const {userId} = req.params
+
+    try{
+const profile = await prisma.profile.findUnique({
+    where: {
+        userId
+    }
+})
+if(profile){
+    res.json({profile})
+}else{
+    res.json({message: "user not found"})
+}
+    }catch(e){
+        console.error(e, "aldaa")
+    }
+});
+usersRouter.get("/auth", async(req:Request, res: Response)=>{
+    // const {username} = req.params
+
+    try{
+const users = await prisma.profile.findMany()
+if(users){
+    res.json({users})
+}else{
+    res.json({message: "user not found"})
+}
+    }catch(e){
+        console.error(e, "aldaa")
+    }
+});
 
 // Testing purposes
 usersRouter.post("/auth/test/login", generateToken);
