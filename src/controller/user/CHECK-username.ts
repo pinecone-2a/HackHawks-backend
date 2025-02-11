d2a3ebbbf19cc7e18832b9211664f5e0ac6af397
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-12 00:33:27 +0800

    update

--- src/controller/user/CHECK-username.ts
+++ src/controller/user/CHECK-username.ts
@@ -16,25 +16,6 @@ export const checkUsername = async (req: Request, res: Response) => {
       res.json({ message: "username is taken", no: "no" });
     } else {
       res.json({ message: "username is available", yes: "yes" });
-      const checkingpass = await bcrypt.compare(password, existingUser.password);
-      if (!checkingpass) {
-        res.json({ message: "user exists, but pass didn't match" });
-        return;
-      }
-      if (email) {
-        const existingProfile = await prisma.profile.findFirst({
-          where: { userId: existingUser.id },
-        });
-        if (existingProfile) {
-          res.json({
-            message: "you have a profile setup",
-            hasProfile: true,
-            id: existingUser.id,
-          });
-          return;
-        }
-      }
-      res.json({ message: "welcome back", id: existingUser.id });
       return;
     }
   } catch (e) {
