4891f2b178b35930b42b6445f6810c98143bc71d
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-11 21:58:29 +0800

    forgot password

--- src/router/usersRouter.ts
+++ src/router/usersRouter.ts
@@ -2,19 +2,20 @@ import { Request, Response, Router } from "express";
 import { loginUser } from "../controller/user/CURRENT-profile";
 import { createUser } from "../controller/user/CREATE-account";
 import { generateToken, verifyToken } from "../controller/authorization/verify";
-import { checkUsername } from "../controller/user/CHECK-username";
 import nodemailer from "nodemailer";
 import { prisma } from "..";
+import bcrypt from "bcrypt";
+import { checkUsername } from "../controller/user/check-username";
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: "smtp.zoho.com",
   port: 465,
-  secure: false, // true for port 465, false for other ports
+  secure: true,
   auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 export const usersRouter = Router();
@@ -25,9 +26,8 @@ usersRouter.post("/auth/sign-in", loginUser);
 
 usersRouter.post("/auth/:username", checkUsername);
 
-// Testing purposes
 usersRouter.post(
-  "/auth/test/reset-password",
+  "/auth/reset-password",
   async (req: Request, res: Response) => {
     const { email } = req.body;
     // res.json({ email });
@@ -40,13 +40,20 @@ usersRouter.post(
       if (user) {
         const otp = Math.floor(Math.random() * 899999999 + 100000000);
 
+        await transporter.sendMail({
+          from: "hackhawks@zohomail.com", // sender address
+          to: email, // list of receivers
+          subject: "Hello ✔", // Subject line
+          text: "Buy me a coffee / Team HackHawks", // plain text body
+          html: `<b>Hello! ${user.username}.</b><p> Here is the OneTimePassword ${otp}</p>`, // html body
+        });
         const newOtp = await prisma.otp.create({
           data: {
             email,
             opt: otp,
           },
         });
-        res.json({ newOtp, success: true });
+        res.json({ success: true, id: newOtp.id });
         return;
       }
       res.json({ success: false });
@@ -56,28 +63,57 @@ usersRouter.post(
     }
   }
 );
+usersRouter.post("/auth/verify-otp", async (req: Request, res: Response) => {
+  const { otp, email, id } = req.body;
+  // res.json({ email });
+  console.log(req.body);
+  try {
+    const user = await prisma.otp.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (user) {
+      if (user.opt === Number(otp) && user.email === email) {
+        res.json({ message: "OTP_MATCHED", success: true });
+        return;
+      }
+      res.json({ success: false, message: "OTP_NOT_MATCHED" });
+      return;
+    }
+    res.json({ success: false, message: "USER_NOT_FOUND" });
+    return;
+  } catch (e) {
+    console.error(e, "aldaa");
+  }
+});
 usersRouter.post(
-  "/auth/test/verify-otp",
+  "/auth/change-password",
   async (req: Request, res: Response) => {
-    const { email } = req.body;
+    const { password, email } = req.body;
     // res.json({ email });
+    console.log(req.body);
     try {
-      const user = await prisma.user.findUnique({
+      const hashedPass = await bcrypt.hash(password, Number(process.env.SALT));
+      const user = await prisma.user.update({
         where: {
           email,
         },
+        data: {
+          password: hashedPass,
+        },
       });
       if (user) {
-        res.json({ user, success: true });
-        return;
+        res.json({ user, message: "amjilttai" });
       }
-      res.json({ success: false });
-      return;
+      console.log(user);
     } catch (e) {
       console.error(e, "aldaa");
     }
   }
 );
+
+// Testing purposes
 
 usersRouter.post("/auth/test/login", generateToken);
 
