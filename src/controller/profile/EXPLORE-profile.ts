9be6e64f4e0508573b3bc72cd26b7455cd1f8cd8
24LP2724 24LP2724 <24LP2724@G302-CC0118.local>
2025-02-13 15:49:29 +0800

    setiings-connect-backend-nasaa

--- src/controller/profile/EXPLORE-profile.ts
+++ src/controller/profile/EXPLORE-profile.ts
@@ -2,17 +2,17 @@ import { Request, Response } from "express";
 import { prisma } from "../..";
 
 export const profileExplore = async (req: Request, res: Response) => {
-  const {search} = req.query
-  
+  const { search } = req.query;
+
   try {
     const exploreData = await prisma.profile.findMany({
-      where:{
-        name:{
+      where: {
+        name: {
           contains: search as string,
-          mode: "insensitive"
-        }
-      }
-    })
+          mode: "insensitive",
+        },
+      },
+    });
     res.json(exploreData);
   } catch (e) {
     console.log(e, "get explore error");
--- src/router/donations.ts
+++ src/router/donations.ts
@@ -1,12 +1,9 @@
 import { Request, Response, Router } from "express";
 import { receivedDonation } from "../controller/donation/GET-donation";
 
-
 export const donationRouter = Router();
 
 donationRouter.post("/create-donation");
 
 // backend dashboard fetch endpoint (ene hesegt buh donation bolon amount days-eer filterdehed heregleh bolomjtoi)//
 donationRouter.get("/:userId", receivedDonation);
-
-
