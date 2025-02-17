86b49cb6487bd05b8363761f8e4d73cc87b04c8a
Duu-Avia <duuavia01@gmail.com>
2025-02-18 03:12:24 +0800

    try to fix logout function

--- src/controller/user/LOGOUT-user.ts
+++ src/controller/user/LOGOUT-user.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 export const logOutUser = async (req: Request, res: Response) => {
-    res.clearCookie("Authorization");
+    res.clearCookie("Authorization",{httpOnly:true, sameSite: "none", secure:true});
 
-    res.clearCookie("RefreshToken");
+    res.clearCookie("RefreshToken", {httpOnly:true, sameSite:"none", secure:true});
     res.json({message: "Хэрэглэгч амжилттай гарлаа", succes:true})
 }
    
