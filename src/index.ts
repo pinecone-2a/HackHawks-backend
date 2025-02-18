8ab7dc39289dab18ea3de60253fd6c31536dcaf9
24LP6048 24LP6048 <24LP6048@G302-CC0121.local>
2025-02-18 18:39:05 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://hackhawks-frontend.vercel.app", "http://localhost:3000"],
     credentials: true,
   })
 );
